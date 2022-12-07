ee631fc660ee553dd145e1735ca8c2f46ba43790
Marcos Alves de Oliveira <marcos.oliveira85@bb.com.br>
2022-12-07 17:24:56 +0000

    Refatoração da classe Main.

--- src/main/java/br/ada/sayajins/Main.java
+++ src/main/java/br/ada/sayajins/Main.java
@@ -5,31 +5,25 @@ import br.ada.sayajins.utils.LeitorCSV;
 import br.ada.sayajins.utils.ProcessarPagamentos;
 import br.ada.sayajins.utils.ThreadGerarCSV;
 
+/*  
+*  @author: Isaac Neves Farias
+*  Geração de arquivos via thread. Uma thread para cada tipo de pagamento.
+*/ 
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        // Instancia o Leitor de CSV de pagamentos
         var leitorCSV = new LeitorCSV();
         
         // Ler pagamentos do arquivo.
-        var listaPagamentos = leitorCSV.lerPagamentos("pagamentos.csv");
+        var pagamentos = leitorCSV.lerPagamentos("pagamentos.csv");
 
         // Realiza o processamento conforme os requisitos.
-        ProcessarPagamentos.processar(listaPagamentos);
-        //System.out.println(listaPagamentos);
-
-        /*
-        * @author: Isaac Neves Farias
-        * Geração de arquivos na main
-        */
-        // Array com a lista de tipos de pagamento
-        TipoPagamentoEnum[] tiposPagamento = {TipoPagamentoEnum.CREDITO, TipoPagamentoEnum.DEBITO, 
-                                              TipoPagamentoEnum.BOLETO, TipoPagamentoEnum.FIDELIDADE, TipoPagamentoEnum.PIX};
+        ProcessarPagamentos.processar(pagamentos);
         
-        
-        
-        // Gera os arquivos CSV por tipo, um em cada thread.                                      
-        for(var tipo : tiposPagamento) {
-            Thread b = new Thread(new ThreadGerarCSV(listaPagamentos, tipo));
+        // Geração dos arquivos por tipo de pagamento
+        for(var tipo : TipoPagamentoEnum.values()) {
+            Thread b = new Thread(new ThreadGerarCSV(pagamentos, tipo));
             b.start();
         }
         
